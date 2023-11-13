938768a0d677e377f01610f103ff5ddf1c312faf
Haytham Elkhoja <haytham@elkhoja.com>
2023-11-13 14:54:00 +0100

    CHANGELOG 1.21 - Updater will run as original user to keep file permissoins consistent - Cleaned up some commenting and documentation - Changed location of repo to https://github.com/kyndryl-open-source/latencyninja

--- inc/variables.sh
+++ inc/variables.sh
@@ -14,7 +14,7 @@
 # along with Latency Ninja.  If not, see <https://www.gnu.org/licenses/>.
 
 # Application Information
-declare -g app_name="Latency Ninja" current_version="1.20"
+declare -g app_name="Latency Ninja" current_version="1.21"
 
 # Updater Information
 declare -g repo_url="https://github.com/kyndryl-open-source/latencyninja.git" version_url="https://raw.githubusercontent.com/kyndryl-open-source/latencyninja/main/version.txt" version_file="version.txt" 
